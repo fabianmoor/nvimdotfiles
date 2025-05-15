eb9bc213d400ea9d35afb7e12e92a459645e1810
Fabian Moor Pucar <fabbem@Fabians-MacBook-Pro.local>
2025-05-15 02:18:16 +0200

    New

--- lua/fabbem/plugins/init.lua
+++ lua/fabbem/plugins/init.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  config = function()
-    require('java').setup()
-  end,
 }
 
 
